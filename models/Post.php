d729d9881ba497a3eea424dc57c90a4625795a00
Hameed Abdulrahaman <hameedabdulrahamann@gmail.comgit config --global user.name Hameed>
2019-10-23 15:17:59 +0100

    get post by id

--- models/Post.php
+++ models/Post.php
@@ -30,6 +30,27 @@ class Post{
         $sth->execute();
         return $sth;
     }
+    public function get_by_id(){
+        $query = ' SELECT c.name as category_name, p.id, p.category_id, p.title,
+        p.body, p.author, p.created_at
+        FROM
+        ' .$this->table .' p
+        LEFT JOIN
+        categories c ON p.category_id = c.id
+        WHERE p.id = ?';
+
+        $sth = $this->conn->prepare($query);
+        $sth->bindParam(1, $this->id);
+        $sth->execute();
+
+        $row = $sth->fetch(PDO::FETCH_ASSOC);
+
+        $this->title = $row['title'];
+        $this->body = $row['body'];
+        $this->author = $row['author'];
+        $this->category_id = $row['category_id'];
+        $this->category_name = $row['category_name']; 
+    }
 
 }
 
