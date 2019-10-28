af1f7c45040246f42a5b6f3b0e618d3ec30cba18
Hameed Abdulrahaman <hameedabdulrahamann@gmail.comgit config --global user.name Hameed>
2019-10-28 12:01:53 +0100

    create categories

---
+++ models/Category.php
@@ -0,0 +1,33 @@
+<?php
+
+class Category{
+
+    private $conn;
+    private $table = 'categories';
+    
+    public $name;
+    public $created_at;
+
+    public function __construct($db){
+        $this->conn = $db;
+    }
+
+    public function create(){
+        $query = 'INSERT INTO '.$this->table.'
+        SET name = :name';
+
+        $sth = $this->conn->prepare($query);
+        
+        $sth->name = htmlspecialchars(strip_tags($this->name));
+
+        $sth->bindParam(':name', $this->name);
+
+        
+        if($sth->execute()){
+            return true;
+        }
+        return false;
+    }
+
+
+}
